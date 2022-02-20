ffb5b0b8989071e7f35338d327ee7f7f803b9618
satoshi aikawa <satoshiaikawa@192.168.0.102>
2022-02-20 16:41:20 +0900

    appbar added.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,32 @@ import {
   Route,
   Link
 } from "react-router-dom";
+
+import axios from 'axios';
+
+
+import Navbar from './components/Navbar/Navbar';
 import Expenses from "./Expenses";
 import Invoices from "./Invoices";
 import Products from "./components/Products/Products"
 import ProductView from "./components/ProductView/productView"
 
-import axios from 'axios';
+
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#330e62',
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 
 
 
@@ -18,33 +38,35 @@ const API_URL='https://httpbin.org'
 
 export default function App() {
 
-  const [mydata, setMydata] = useState({});
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetchData();
+    fetchProductsData();
   }, []);
 
-  const fetchData = () => {
-    axios.get(API_URL+`/brotli`)
+  const fetchProductsData = () => {
+    //axios.get(API_URL+`/brotli`)
+    axios.get(`http://localhost:3001/api/products`)
     .then(res => {
       console.log(res.data)
-      setMydata(res.data)
+      setProducts([...res.data])
     })
   };
 
   return (
     <div>
+  <ThemeProvider theme={theme}>
   <BrowserRouter>
+    <Navbar/>
     <Routes>
-      <Route exact path="/" element={<App />} />
-      <Route exact path="/pro" element={<Products products={products} />} />
+      <Route exact path="/" element={<Products products={products} />} />
       <Route path="/cart" element={<Expenses />} />
       <Route path="/checkout" element={<Invoices />} />
       <Route path="/product-view/:id" element={<ProductView />} />
       <Route path="/invoices" element={<Invoices />} />
     </Routes>
   </BrowserRouter>
+  </ThemeProvider>
     </div>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,4 +1,11 @@
-export default function Expenses() {
+import React, { useState, useEffect } from 'react';
+
+export default function Products({ products, onAddToCart }) {
+
+    useEffect(() => {
+        console.log(products)
+      }, []);
+
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>products </h2>
