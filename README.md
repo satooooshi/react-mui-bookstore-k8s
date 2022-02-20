76117c1ee8038f7da3094d36b9bdcd0210784170
satoshi aikawa <satoshiaikawa@192.168.0.102>
2022-02-20 15:39:52 +0900

    v6 routing ok

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 #  online bookstore with React+MUI+k8s
 
+https://reactrouter.com/docs/en/v6/getting-started/overview
+https://mui.com/getting-started/installation/
+
 npx create-react-app react-mui-bookstore-k8s
 npm install react-router-dom@6
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+
 import Button from '@mui/material/Button';
 
-function App() {
-  return <Button variant="contained">Hello World</Button>;
-}
+import { Link } from "react-router-dom";
 
-export default App;
+
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link> |{" "}
+        <Button variant="contained">Hello World</Button>
+      </nav>
+    </div>
+  );
+}
