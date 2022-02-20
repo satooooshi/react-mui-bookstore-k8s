6c2920a19a3f468b5c6c8f206b8af6b840034c86
satoshi aikawa <satoshiaikawa@192.168.0.102>
2022-02-20 15:50:06 +0900

    axios test ok

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 https://reactrouter.com/docs/en/v6/getting-started/overview
 https://mui.com/getting-started/installation/
+https://www.npmjs.com/package/axios
+https://httpbin.org/#/HTTP_Methods
+curl -X GET "https://httpbin.org/brotli" -H "accept: application/json"
 
 npx create-react-app react-mui-bookstore-k8s
-npm install react-router-dom@6
+
+npm start
 
 
 
--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import Button from '@mui/material/Button';
 
 import { Link } from "react-router-dom";
 
+import axios from 'axios';
 
 
 
+const API_URL='https://httpbin.org'
 
 export default function App() {
+
+  const [mydata, setMydata] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios.get(API_URL+`/brotli`)
+    .then(res => {
+      console.log(res.data)
+      setMydata(res.data)
+    })
+  };
+
   return (
     <div>
       <h1>Bookkeeper</h1>
